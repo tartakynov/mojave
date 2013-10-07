78f211dbdb70ccab512f1288125f560dbe6cf472
Artem Tartakynov <artem.tad@gmail.com>
2013-10-07 23:18:23 +0700

    second

--- mojave-executable/src/main/java/com/github/tartakynov/mojave/App.java
+++ mojave-executable/src/main/java/com/github/tartakynov/mojave/App.java
@@ -1,13 +1,21 @@
 package com.github.tartakynov.mojave;
 
 
+import org.mozilla.javascript.Context;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) throws Exception {
+public class App {
+    public static void main(String[] args) throws Exception {
+        try {
+            Context context = Context.enter();
+            context.setOptimizationLevel(9);
 
+            Global global = new Global(context, false);
+            global.run("c:\\src\\test.js");
+        } finally {
+            Context.exit();
+        }
     }
 }
